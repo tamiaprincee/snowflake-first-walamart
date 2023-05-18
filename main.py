79f36b46b05020bea02b9d75c4649d26c74831c1
Roman Tafelski <roman.tafelski@allata.com>
2023-05-18 14:37:16 -0400

    Testing azure again

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ from langchain.vectorstores import FAISS
 from utils.snowddl import Snowddl
 from utils.snowchat_ui import reset_chat_history, extract_code, message_func, is_sql_query
 
+# MAX_INPUTS = 3 because gpt-3.5-turbo only allows 3 per minute.
+
 openai.api_key = st.secrets["OPENAI_API_KEY"]
-MAX_INPUTS = 5
+MAX_INPUTS = 3
 chat_history = []
 
 st.set_page_config(
