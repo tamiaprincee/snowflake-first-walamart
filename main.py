d38fe830fbcecf70685a1b81b51767d2189dc5c2
Roman Tafelski <roman.tafelski@allata.com>
2023-05-18 15:42:09 -0400

    adding secrets

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from utils.snowchat_ui import reset_chat_history, extract_code, message_func, is
 # MAX_INPUTS = 3 because gpt-3.5-turbo only allows 3 per minute.
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
-MAX_INPUTS = 3
+MAX_INPUTS = 4
 chat_history = []
 
 st.set_page_config(
